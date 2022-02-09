37c0a0b8beb051fecc0ac2b72912dc39a7fc638d
abhiram11 <abhiramsatpute@gmail.com>
2022-02-09 14:38:11 +0530

    working on content textual

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Contact from "./components/Contact";
 import Header from "./components/Header";
 import Intro from "./components/Intro";
 import Projects from "./components/Projects";
+import TextualContent from "./components/TextualContent";
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Intro />
+      <TextualContent />
       {/* <Projects /> */}
       {/* <Projects />
       <WorkEx />
---
+++ src/components/TextualContent/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./TextualContent.css";
+
+function index() {
+  return (
+    <div className="textual__content">
+      <h2>Hiiiiiiii</h2>
+      <p>
+        I am Abhiram, an experienced Data Scientist in Real Estate and
+        Healthcare Industries, and now also a budding Full-Stack Developer!
+      </p>
+      <p>
+        I have worked as a Data Scientist at two startups comprising of elite
+        individuals, hBits (team of around 12, with grads from Harvard,
+        Stanford, Duke, IIT Madras and Bombay, IIM Ahmedabad and Indore, NIT
+        Nagpur, and other entrepreneurs), and mBreath Technologies (team of 4
+        with myself being the first full-time hire, two IIT Kharagpur PhD
+        Scholars and an IIT Kharagpur Professor, along with interns from IIT
+        Kharagpur, NIT Raipur, and VIT).
+      </p>
+    </div>
+  );
+}
+
+export default index;
