f36515e4ad7949cdd84b2468ec6b0086db37e6e4
abhiram11 <abhiramsatpute@gmail.com>
2022-02-11 13:17:35 +0530

    exp improved and divided into parts

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -1,15 +1,33 @@
 import React from "react";
 import "./Experience.css";
+import pic1 from "../../media/about.jpg";
 
 function Experience() {
   return (
     <div>
       <h2>Experience</h2>
       <div className="experience__wrapper">
-        <div className="experience__wrapper__a">hi</div>
+        <div className="experience__wrapper__a">
+          {/* just change their flex start etc stuff.... */}
+          <div className="experience__wrapper__content">
+            <h4>hBits, Nariman Point, Mumbai</h4>
+            <p>Word word word</p>
+          </div>
+          <div className="experience__wrapper__image">
+            <img src={pic1} alt="hBits" />
+          </div>
+        </div>
 
-        <div className="experience__wrapper__b">hi</div>
-        <div className="experience__wrapper__a">hi</div>
+        <div className="experience__wrapper__b">
+          <div className="experience__wrapper__content">
+            <h4>mBreath Technologies, IIT Kharagpur (E-Cell)</h4>
+            <p>Word word word</p>
+          </div>
+          <div className="experience__wrapper__image">
+            <img src={pic1} alt="hBits" />
+          </div>
+        </div>
+        {/* <div className="experience__wrapper__a">hi</div> */}
       </div>
     </div>
   );
