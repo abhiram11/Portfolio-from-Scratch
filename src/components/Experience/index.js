befb521e91d62d6d3b680cc4fd242b09719374f7
abhiram11 <abhiramsatpute@gmail.com>
2022-02-11 19:30:46 +0530

    styled and aligned, content edited again (alot)

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -3,35 +3,59 @@ import "./Experience.css";
 import qdic from "../../media/about.jpg";
 import iimcip from "../../media/IIMCIP.jpg";
 import titan from "../../media/Titan.jpg";
+// import prize from "../../media/img_bg_1.jpg";
 
 function Experience() {
   return (
-    <div id="experience">
+    <div className="experience" id="experience">
       <h2>Work Experience</h2>
-      <div className="experience__wrapper">
-        <div className="experience__wrapper__a">
-          <div className="experience__wrapper__content">
-            <h4>Data Scientist, hBits Online Platform Services</h4>
-            <p className="experience__wrapper__duration"></p>
-            <p>Word word word</p>
-          </div>
+      <div className="experience__wrapper__upper">
+        <div className="experience__wrapper__upper__a">
+          <h4>Data Scientist, hBits Online Platform Services</h4>
+          <p className="experience__wrapper__duration">Sept 2019 - Jan 2020</p>
+          <p>
+            • Mapped the Real Estate (structured and unstructured) data of a
+            whole state of India using scraping algorithms and OCR, analyzed
+            them using SpaCy, NLTK, and coreNLP to extract the required
+            insights.
+            <br />
+            <br />• Applied AWS Services (Amazon Textract, S3, and more) and NLP
+            algorithms to convert other unstructured datasets in the form of
+            images and PDF Documents into structured spreadsheets and tables to
+            get better insights regarding pricing, amenities, location, and
+            other factors essential for Real Estate pricing.
+          </p>
         </div>
-
-        <div className="experience__wrapper__b">
-          <div className="experience__wrapper__content">
-            <h4>Data Scientist, mBreath Technologies</h4>
-            <p className="experience__wrapper__duration"></p>
-            <p>Word word word</p>
-          </div>
+        <div className="border" />
+        <div className="experience__wrapper__upper__a">
+          <h4>Data Scientist, mBreath Technologies</h4>
+          <p className="experience__wrapper__duration">Sept 2018 - July 2019</p>
+          <p>
+            • Researched and Developed three data science projects real-time
+            audio analysis and classification, sleep apnea detection, and sleep
+            stage classification.
+            <br />
+            <br />
+            • Worked on assisting the CxOs at multiple fronts including Business
+            Model Development, Customer Acquisition, Market Analysis, Product
+            Feasibility Analysis and Customer Segmentation and Satisfaction
+            analysis while working as Data Scientist.
+            <br />
+            <br />• Drafting these projects as research papers as the co-author
+            while at IIT Kharagpur.
+          </p>
         </div>
       </div>
+      {/* <img src={prize} alt="prize" /> */}
       <h2>Competitions and Entrepreneurship Experiences</h2>
-      <div className="experience__wrapper">
+      <div>
         <div className="experience__wrapper__a">
           {/* just change their flex start etc stuff.... */}
-          <div className="experience__wrapper__content">
+          <div className="experience__wrapper__lower">
             <h4>Titan Technology Tune-in 2.0, Tamil Nadu</h4>
-            <p className="experience__wrapper__duration">December 2020- 2021</p>
+            <p className="experience__wrapper__duration">
+              April 2019 - May 2019
+            </p>
             <p>
               This forum enabled us to showcase our product and technology to
               TITAN senior management (Director, Managing Director, CEO, and
@@ -49,9 +73,11 @@ function Experience() {
           </div>
         </div>
         <div className="experience__wrapper__b">
-          <div className="experience__wrapper__content">
+          <div className="experience__wrapper__lower">
             <h4>Health Trail Programme, IIM Calcutta</h4>
-            <p className="experience__wrapper__duration">December 2020- 2021</p>
+            <p className="experience__wrapper__duration">
+              Jan 2019 - April 2019
+            </p>
 
             <p>
               Health Trail was a three month non-residential accelerator
@@ -68,9 +94,11 @@ function Experience() {
         </div>
         <div className="experience__wrapper__a">
           {/* just change their flex start etc stuff.... */}
-          <div className="experience__wrapper__content">
-            <h4>Qualcomm Design in India Challenge</h4>
-            <p className="experience__wrapper__duration">December 2020- 2021</p>
+          <div className="experience__wrapper__lower">
+            <h4>Qualcomm Design in India Challenge, Bangalore</h4>
+            <p className="experience__wrapper__duration">
+              Sept 2019 - March 2019
+            </p>
             <p>
               This was one of the toughest startup programme to get selected.
               Our startup, mBreath, got ranked in Top 15 Nationwide Startups in
