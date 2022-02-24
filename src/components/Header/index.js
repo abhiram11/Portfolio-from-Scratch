91a43083171027c88aa3346ff77fa3a70500239c
abhiram11 <abhiramsatpute@gmail.com>
2022-02-24 08:17:14 +0530

    app css was altered to test background... now undo-ed

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,6 +3,8 @@ import "./Header.css";
 import { Link as LinkS } from "react-scroll";
 // import { Link } from "react-router-dom";
 
+// navigation not working as expected....some errors happening and takes action after scrolling a bit below
+
 function Header() {
   return (
     <div className="header">
